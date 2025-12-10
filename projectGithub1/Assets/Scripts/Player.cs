e7c304732899abb3f95f430cba090bedd54c3240
tukan-istukan <tukicvlad@gmail.com>
2025-12-10 19:14:45 +0700

    Player new action
    движение на стрелочках

--- projectGithub1/Assets/Scripts/Player.cs
+++ projectGithub1/Assets/Scripts/Player.cs
@@ -12,11 +12,11 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.D)) 
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) 
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
         }
